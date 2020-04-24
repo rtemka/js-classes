24b99a034ec5615c9b3d16afe8147da28e934031
Artem Rybakov <gizmo3000@yandex.ru>
2020-04-24 14:14:50 +0300

    complete core logic. JS is a mess!

---
+++ README.md
@@ -0,0 +1,5 @@
+## js-classes
+
+### This is the sixth project from [this course](https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced)
+
+#### JavaScript classes exercise.
---
+++ assets/scripts/app.js
@@ -0,0 +1,181 @@
+class Product {
+  constructor(title, price, image, desc) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = image;
+    this.description = desc;
+  }
+}
+
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
+  }
+
+  render() {}
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
+  items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.TotalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log("Ordering...");
+      console.log(this.items);
+    };
+    this.render();
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
+  render() {
+    const cartEl = this.createRootElement("section", "cart");
+    cartEl.innerHTML = `
+            <h2>Total: \$${0}</h2>
+            <button>Order now!</button>
+        `;
+    const orderBtn = cartEl.querySelector("button");
+    orderBtn.addEventListener("click", this.orderProducts);
+    this.TotalOutput = cartEl.querySelector("h2");
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
+    this.product = product;
+    this.render();
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = this.createRootElement("li", "product-item");
+    prodEl.innerHTML = `
+      <div>
+      <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+      <div class="product-item__content">
+        <h2>${this.product.title}</h2>
+        <h3>\$${this.product.price}</h3>
+        <p>${this.product.description}</p>
+        <button>Add to Cart</button>
+      </div>
+    </div>
+      `;
+    const addCartBtn = prodEl.querySelector("button");
+    addCartBtn.addEventListener("click", this.addToCart.bind(this));
+  }
+}
+
+class ProductList extends Component {
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.products = [
+      new Product(
+        "A Pillow",
+        19.99,
+        "https://www.potterybarn.com/pbimgs/rk/images/dp/wcm/201826/0010/faye-textured-linen-pillow-covers-m.jpg",
+        "A soft pillow!"
+      ),
+      new Product(
+        "A Carpet",
+        89.99,
+        "https://i.ytimg.com/vi/bG5C-A7BgOg/maxresdefault.jpg",
+        "A carpet which you might like - or not."
+      ),
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, "prod-list");
+    }
+  }
+
+  render() {
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
+    }
+  }
+}
+
+class Shop extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    this.cart = new ShoppingCart("app");
+    new ProductList("app");
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
